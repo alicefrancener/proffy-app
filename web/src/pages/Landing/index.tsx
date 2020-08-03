b942debc28ce8cfb151861b13c2c6f8eb95a918f
Alice Francener <alicefrancener@gmail.com>
2020-08-03 11:03:42 -0300

    feat: cria estrutura landing page

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import logoImg from "../../assets/images/logo.svg";
+import ladingImd from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcons from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+function Landing() {
+  return (
+    <div id="page">
+      <div id="page-landing-content" className="container">
+        <div id="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
+        <img
+          src={ladingImd}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+          <a href="">
+            <img src={giveClassesIcons} alt="" />
+            Estudar
+          </a>
+        </div>
+        <span className="total-connections">
+          Total de 200 conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;

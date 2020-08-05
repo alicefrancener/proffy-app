d0a0cff3ae4146d697c9ba70b4769e4d47272cb8
Alice Francener <alicefrancener@gmail.com>
2020-08-05 17:34:34 -0300

    feat: add new select on click

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -10,6 +10,14 @@ import warningIcon from "../../assets/images/icons/warning.svg";
 import "./styles.css";
 
 function TeacherForm() {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 1, from: "", to: "" },
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems, { week_day: 1, from: "", to: "" }]);
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -62,41 +70,31 @@ function TeacherForm() {
 
         <fieldset>
           <legend>
-            Horários disponíveis <button type="button">+ Novo horário</button>
+            Horários disponíveis
+            <button type="button" onClick={addNewScheduleItem}>+ Novo horário</button>
           </legend>
-          <div className="schedule-item">
-            <Select
-              name="subject"
-              label="Matéria"
-              options={[
-                {
-                  value: "Algoritmos e estrutura de dados",
-                  label: "Algoritmos e estrutura de dados",
-                },
-                {
-                  value: "Construção de software",
-                  label: "Construção de software",
-                },
-                { value: "Design de software", label: "Design de software" },
-                {
-                  value: "Introdução à programação",
-                  label: "Introdução à programação",
-                },
-                { value: "Lógica matemática", label: "Lógica matemática" },
-                { value: "Matemática discreta", label: "Matemática discreta" },
-                {
-                  value: "Programação orientada a objetos",
-                  label: "Programação orientada a objetos",
-                },
-                {
-                  value: "Redes de computadores",
-                  label: "Redes de computadores",
-                },
-              ]}
-            />
-            <Input name="from" label="Das" type="time" />
-            <Input name="to" label="Até" type="time" />
-          </div>
+
+          {scheduleItems.map((scheduleItem) => {
+            return (
+              <div key={scheduleItem.week_day} className="schedule-item">
+                <Select
+                  name="week_day"
+                  label="Dia da semana"
+                  options={[
+                    { value: "0", label: "Domingo" },
+                    { value: "1", label: "Segunda-feira" },
+                    { value: "2", label: "Terça-feira" },
+                    { value: "3", label: "Quarta-feira" },
+                    { value: "4", label: "Quinta-feira" },
+                    { value: "5", label: "Sexta-feira" },
+                    { value: "6", label: "Sábado" },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+              </div>
+            );
+          })}
         </fieldset>
 
         <footer>

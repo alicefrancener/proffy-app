67582b58befe0713bbde53d5ee102ecbc5a1f7b1
Alice Francener <alicefrancener@gmail.com>
2020-08-05 07:33:08 -0300

    feat: add get route to connection

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -8,8 +8,9 @@ export default class ConnectionsController {
   async create(request: Request, response: Response) {
     const { user_id } = request.body;
 
-    await db("connections").insert({ user_id });
+    const insertedConnections = await db("connections").insert({ user_id });
+    const connection_id = insertedConnections[0];
 
-    return response.status(201);
+    return response.status(201).send({ connection_id });
   }
 }

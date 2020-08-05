b45d8f5c0615a3388e35d051b32010bebc6162f6
Alice Francener <alicefrancener@gmail.com>
2020-08-05 14:25:14 -0300

    feat: add Textarea component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
+
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 import "./styles.css";
 
@@ -16,10 +19,26 @@ function TeacherForm() {
       <main>
         <fieldset>
           <legend>Seus dados</legend>
-          <Input name="name" label="Nome completo"/>
-          <Input name="avatar" label="Avatar"/>
-          <Input name="whatsapp" label="WhatsApp"/>
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia"/>
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula (em R$)" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );

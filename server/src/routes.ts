179e13b8eff7298f96693a435d0d67274d9e4a38
Alice Francener <alicefrancener@gmail.com>
2020-08-05 07:22:19 -0300

    feat: add routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import ClassesControler from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = express.Router();
 const classesControllers = new ClassesControler();
+const connectionsControllers = new ConnectionsController();
 
 routes.post("/classes", classesControllers.create);
+routes.get("/classes", classesControllers.index);
+
+routes.post("/connections", connectionsControllers.create);
+routes.get("/connections", connectionsControllers.index);
 
 export default routes;

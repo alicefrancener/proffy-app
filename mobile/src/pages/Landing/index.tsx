c5c4ac82752e53847e9d749e399a9259a31830ec
Alice Francener <alicefrancener@gmail.com>
2020-08-08 10:44:29 -0300

    feat: navigate from landing page to giveClasses page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { View, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
+
 import styles from "./styles";
 
 import landingImg from "../../assets/images/landing.png";
@@ -8,6 +11,12 @@ import giveClasses from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 
 function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate("GiveClasses");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -17,20 +26,22 @@ function Landing() {
         <Text style={styles.titleBold}>O que deseja fazer?</Text>
       </Text>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
           <Image source={giveClasses} />
           <Text style={styles.buttonText}>Dar aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de de 280 conexões já realizadas {' '}
-        <Image source={heartIcon}/>
+        Total de de 280 conexões já realizadas <Image source={heartIcon} />
       </Text>
     </View>
   );

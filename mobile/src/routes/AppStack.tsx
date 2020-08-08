58e285b3ea4a2105bab7b198a6ad75ee6b079153
Alice Francener <alicefrancener@gmail.com>
2020-08-08 12:39:07 -0300

    feat: add navigation in tabs

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,7 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Landing from "../pages/Landing";
-import GiveClasses from '../pages/GiveClasses';
+import GiveClasses from "../pages/GiveClasses";
+import StudyTabs from "./StudyTabs";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,6 +14,7 @@ function AppStack() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Lading" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
+        <Screen name="Study" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
   );

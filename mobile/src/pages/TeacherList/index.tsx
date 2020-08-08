fd2eae8a4da290cf8dc3eb2fe966b123ea32d308
Alice Francener <alicefrancener@gmail.com>
2020-08-08 12:55:51 -0300

    feat: add Tabs

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 
+import styles from './styles';
+
 function TeacherList() {
-  return <View></View>;
+  return <View style={styles.container}></View>;
 }
 
 export default TeacherList;

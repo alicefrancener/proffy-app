e710f6b651b366ae8cf747dd9a712f83e8285748
Alice Francener <alicefrancener@gmail.com>
2020-08-08 16:37:19 -0300

    feat: add scroll to TeacherItem

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { View } from "react-native";
 
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 import styles from "./styles";
+import { ScrollView } from "react-native-gesture-handler";
 
 function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+    <ScrollView style={styles.teacherList} contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 16}}>
+    <TeacherItem  />
+      <TeacherItem  />
+      <TeacherItem  />
+      <TeacherItem  />
+    </ScrollView>
     </View>
   );
 }

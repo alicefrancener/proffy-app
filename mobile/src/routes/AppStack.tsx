64d79682115d4c15cc323c1ccf765da3c0db9dc7
Alice Francener <alicefrancener@gmail.com>
2020-08-08 14:57:07 -0300

    feat: add PageHeader component

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -12,7 +12,7 @@ function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="Lading" component={Landing} />
+        <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
         <Screen name="Study" component={StudyTabs} />
       </Navigator>

6de20bace42bf6cc29a05acedb142c72225fa6f9
Alice Francener <alicefrancener@gmail.com>
2020-08-08 20:06:04 -0300

    style: Inputs

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -1,14 +1,57 @@
-import { StyleSheet  } from "react-native";
+import { StyleSheet } from "react-native";
 
-const styles =  StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    backgroundColor: '#f0f0f7',
+    flex: 1,
+    backgroundColor: "#f0f0f7",
   },
 
   teacherList: {
-    marginTop: -40
-  }
+    marginTop: -40,
+  },
+
+  searchForm: {
+    marginBottom: 24,
+  },
+
+  label: {
+    color: "#d4c2ff",
+    fontFamily: "Poppins_400Regular",
+  },
+
+  inputGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  inputBlock: {
+    width: "48%",
+  },
+
+  input: {
+    height: 54,
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16,
+  },
+
+  submmitButton: {
+    backgroundColor: "#04d361",
+    height: 56,
+    flexDirection: "row",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  submmitButtonText: {
+    color: "#FFF",
+    fontFamily: "Archivo_700Bold",
+    fontSize: 16,
+  },
 });
 
 export default styles;
